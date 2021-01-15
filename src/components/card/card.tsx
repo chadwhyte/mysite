af266fc6f976c4b6da37e8787a7298072043829b
Chad Whyte <chad.whyte@gmail.com>
2021-01-14 21:08:16 -0700

    Update card styling

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -4,15 +4,17 @@ import { ICard } from './card.interface';
 function Card(card : ICard) {
     return (
         <div className="card-container">
-            <h1>
+            <h2>
                 {card.title}
-            </h1>
-            <span className="material-icons card-logo">
-                {card.icon};
-            </span>
-            <a className="card-link" href={card.link} target="_blank">
-                {card.text}
-            </a>
+            </h2>
+            <div className="card-details">
+                <span className="material-icons card-logo">
+                    {card.icon}
+                </span>
+                <a className="card-link" href={card.link} target="_blank">
+                    {card.text}
+                </a>
+            </div>
         </div>
     );
 }
