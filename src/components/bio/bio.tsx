dcfb3b1bcc3579d6821afb3090539e569fc8d1e1
Chad Whyte <chad.whyte@gmail.com>
2021-01-30 08:16:05 -0700

    Update readme

--- src/components/bio/bio.tsx
+++ src/components/bio/bio.tsx
@@ -2,7 +2,7 @@ function Bio() {
     return (
         <div>
             <p>
-            TODO - turn this bio section into a collection of "tag" styled components.  This won't be a traditional bio, but a collection of tags describing myself.  Allow clicking of tags to support basic react state hooks.  
+            TODO
             </p>
         </div>
     );
